b0de4131be4424e312ebb5fc6eccdd0c9d2f736a
quinno <qman.personal@gmail.com>
2023-06-04 16:34:35 -0500

    Added test data/reference

--- algorithm.py
+++ algorithm.py
@@ -8,3 +8,4 @@ data = {
 
 # TO DO: want to create a song reccomendation algorithm that goes off some interesting piece of data
 # |-> thinking political quiz scores, easiest political compass: enter the values you got from the quiz then see what the algorithm would reccomend
+# Getting some guidance from: https://towardsdatascience.com/a-simple-song-recommender-system-in-python-tutorial-3e4c111198d6
