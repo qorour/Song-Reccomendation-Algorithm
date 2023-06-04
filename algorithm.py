8ec749306110f0ed7b8f9dc2a6db5e7d6d9f656b
qorour <qo.personalbusiness@gmail.com>
2023-06-04 16:45:03 -0500

    loading data/testing printing

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,6 @@
 import requests
 import base64
+import pandas as pd
 
 # Set the headers and data
 
@@ -9,3 +10,6 @@ data = {
 # TO DO: want to create a song reccomendation algorithm that goes off some interesting piece of data
 # |-> thinking political quiz scores, easiest political compass: enter the values you got from the quiz then see what the algorithm would reccomend
 # Getting some guidance from: https://towardsdatascience.com/a-simple-song-recommender-system-in-python-tutorial-3e4c111198d6
+
+df = pd.read_csv('test-ratings-data.csv') 
+print(df)
