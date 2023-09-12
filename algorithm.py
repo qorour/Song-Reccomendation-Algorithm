a332a0f907f73fcc54ea90a3eed384d202df4ca2
qorour <qo.personalbusiness@gmail.com>
2023-09-11 21:42:49 -0500

    added manual input, more organized, going to allow new users to add themselves to data

--- algorithm.py
+++ algorithm.py
@@ -23,47 +23,37 @@ my_ratings = { 'Name':'Quinn',
 }
 
 # TO DO: looks like appending new rating completely messes up index column. Find out how to add new entries easily.
-print(my_ratings)
 #df = df.append(my_ratings, ignore_index = True)
-print(df)
+#print(df)
 ratings=df.fillna(3) # filling all nulls w/3 vals (neutral) so doesn't ruin formula
-
+closest_distance=float('inf')
 
 def distance_form(person1,person2): # used to find person closest to song ratings to then help make predictions on new entry
-  print("distance form time")
-  print(person2)
-  print(ratings.loc[person2])
   p1 = ratings.loc[person1]
   p2 = ratings.loc[person2]
   c = euclidean(p1,p2)
   return c
 
 def closest_person(person):
+  # Check if person exists in dataset
+  if person not in ratings.index:
+    return "ERROR: user not registered"
   current = ratings.loc[person]
   closest_person=''
+  global closest_distance
   closest_distance=float('inf')
 
   for p in ratings.index:
-    print("curr p1:")
-    print(p)
-    print("curr p2:")
-    print(current)
-    print("curr p3:")
-    print(person)
     if p==person:
-      print("curr p continue found")
       continue # if looking at current person, continue past them to find closest person that's not themself
-    print("curr not the same one/is different")
     distance_to_other_person = distance_form(person,p)
     if distance_to_other_person < closest_distance:
-      print("curr p lower found")
       # new highest, so make it the new closest distance
       closest_distance = distance_to_other_person
       closest_person = p
 
   return closest_person
 
-closest_distance=float('inf')
-print("I'm most similar to:")
-print(closest_person('Farmer Pete'))
-print(closest_distance)
+name = input('What is your name?\n')
+print("You are most similar to: ", closest_person(name))
+print("with a similarity distance of: ", closest_distance)
