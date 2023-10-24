bb8158c281f9dc22ce1005d752d1ee117574b331
qorour <qo.personalbusiness@gmail.com>
2023-10-24 14:03:43 -0500

    starting work on political scores tracking, will integrate with song reccomender for analysis/make reccomendations based off one

---
+++ politic.py
@@ -0,0 +1,3 @@
+# Help on how to graph/quantify political opinions from: https://www.storybench.org/ten-ways-you-might-consider-visualizing-political-issues-and-ideologies-this-election/
+
+# TO DO: Write basic class outline that stores a person's name, politic allignment, and position on political compass or however I best think is right data
